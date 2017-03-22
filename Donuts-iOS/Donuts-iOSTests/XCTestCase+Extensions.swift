ea800828c1010ba063224147a1f9ddf9b4533622
Joshua Kovach <kovach.jc@gmail.com>
2017-03-22 12:52:08 -0400

    Fixup extension method

--- Donuts-iOS/Donuts-iOSTests/XCTestCase+Extensions.swift
+++ Donuts-iOS/Donuts-iOSTests/XCTestCase+Extensions.swift
@@ -9,20 +9,22 @@
 import XCTest
 import Foundation
 
-func fixtureJson(name: String) -> [String: Any]? {
-  // Find json file with the given name from the main bundle.
-  if let path = Bundle(for: UserTest.self).path(forResource: name, ofType: "json") {
-    // Convert the path of the file to a Data object so we can convert it to JSON.
-    if let data = try? Data(referencing: NSData(contentsOfFile: path)) {
-      // Serialize the data into JSON and handle some basic errors.
-      do {
-        return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
-      } catch {
-        XCTFail("\(error.localizedDescription)")
+extension XCTestCase {
+  func fixtureJson(name: String) -> [String: Any]? {
+    // Find json file with the given name from the main bundle.
+    if let path = Bundle(for: UserTest.self).path(forResource: name, ofType: "json") {
+      // Convert the path of the file to a Data object so we can convert it to JSON.
+      if let data = try? Data(referencing: NSData(contentsOfFile: path)) {
+        // Serialize the data into JSON and handle some basic errors.
+        do {
+          return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+        } catch {
+          XCTFail("\(error.localizedDescription)")
+        }
       }
+    } else {
+      XCTFail("Can't find the test JSON file")
     }
-  } else {
-    XCTFail("Can't find the test JSON file")
+    return nil
   }
-  return nil
 }
